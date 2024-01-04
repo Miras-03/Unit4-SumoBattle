9b22a0be9fdd526bd42415b965f442e2b9dc3f83
Miras Bolatbekuly <mynameismiras2003@gmail.com>
2024-01-04 12:34:00 +0300

    Implemented player control and camera rotate around functionality

---
+++ SumoBattle/Assets/_Scripts/CameraRotation.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private const int rotationSpeed = 45;
+
+    private void FixedUpdate() => CheckOrRotate();
+
+    private void CheckOrRotate()
+    {
+        if (GetHorizontalInput() != 0)
+            Rotate();
+    }
+
+    private void Rotate() => transform.Rotate(Vector3.up, GetHorizontalInput() * -rotationSpeed * Time.fixedDeltaTime);
+
+    private float GetHorizontalInput() => Input.GetAxis("Horizontal");
+}
---
+++ SumoBattle/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform focusCentre;
+    private Rigidbody rb;
+    private const int moveSpeed = 400;
+
+    private void Awake() => rb = GetComponent<Rigidbody>();
+
+    private void FixedUpdate() => CheckOrMove();
+
+    private void CheckOrMove()
+    {
+        if (GetVerticalInput() != 0)
+            Move();
+    }
+
+    private void Move() => rb.AddForce(focusCentre.forward * GetVerticalInput() * moveSpeed * Time.fixedDeltaTime);
+
+    private float GetVerticalInput() => Input.GetAxis("Vertical");
+}
